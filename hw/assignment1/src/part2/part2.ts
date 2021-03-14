ea028a718a43b3d3ea4045c365a3de037a6fa4c5
NoaP <noahp@post.bgu.ac.il>
2021-03-14 12:38:03 +0200

    update 2

--- hw/assignment1/src/part2/part2.ts
+++ hw/assignment1/src/part2/part2.ts
@@ -4,14 +4,17 @@ const stringToArray = R.split("");
 
 /* Question 1 */
 export const countVowels = (str: string): number =>{
-    const arr:string []= stringToArray(str);
-    const result:string[] = arr.filter((s:string):boolean =>( s==='o'||s==='i'|| s==='a' || s==='e' ||
-    s==='u' || s==='O'||s==='I'|| s==='A'|| s==='E' || s==='U'));
-    return result.reduce((acc:number, curr:string):number=>(acc+1),0);
+    const a:string = str.toUpperCase();
+    const arr:string []= stringToArray(a);
+    const result:string[] = arr.filter((s:string):boolean =>(s==='O'||s==='I'|| s==='A'|| s==='E' || s==='U'));
+    return result.length;
+    //return result.reduce((acc:number, curr:string):number=>(acc+1),0);
 }
 
 /* Question 2 */
-export const runLengthEncoding = undefined;
+export const runLengthEncoding = (str:string): string =>{
+    const arr: string[] = stringToArray(arr);
+}
 
 /* Question 3 */
 export const isPaired = undefined;
