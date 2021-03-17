975d116604c84efd412edcada1b02f29275201d0
Nir <nirsel@post.bgu.ac.il>
2021-03-17 21:01:22 +0200

    bind

--- hw/assignment1/src/part3/state.ts
+++ hw/assignment1/src/part3/state.ts
@@ -1,13 +1,12 @@
-import { compose, pipe } from "ramda";
+import { apply, compose, pipe } from "ramda";
 
 export type State<S, A> = (initialState: S) => [S, A];
 
 export const bind: <S, A, B>(state: State<S, A>, f: (x: A) => State<S, B>) => State<S, B> = <S, A, B>(state: State<S, A>,f: (x:A)=>State<S, B>) => {
-    const func = pipe(
-        (x: S) => state(x),
-        (x: [S,A]) => f(x[1])
-    );
-    return func();
+    const func = (x:S):[S,B] =>{
+        return f(state(x)[1])(x);
+    };
+    return func;
 }
 
 
