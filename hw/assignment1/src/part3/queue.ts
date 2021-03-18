2338c5e5a7c3500264437639ddc399aba8367afc
Nir <nirsel@post.bgu.ac.il>
2021-03-18 13:49:53 +0200

    update

--- hw/assignment1/src/part3/queue.ts
+++ hw/assignment1/src/part3/queue.ts
@@ -2,8 +2,22 @@ import { State, bind } from "./state";
 
 export type Queue = number[];
 
-export const enqueue = undefined;
+export const enqueue:(x: number)=>State<Queue,undefined> = (x:number)=>{
+    const func=(q:Queue):[Queue,undefined]=>{
+        return [q.concat(x),undefined];
+    }
+    return func;
+} ;
 
-export const dequeue = undefined;
+export const dequeue:State<Queue,number> = (q:Queue)=>{
+    return [q.slice(1,q.length),q[0]];
+};
 
-export const queueManip = undefined;
+//export const queueManip:State<Queue,number> = (q:Queue)=>{
+ //   return bind(dequeue,(x:number)=>bind(enqueue(x*2),(y:undefined)=>bind(enqueue(x/3),(z:undefined)=>dequeue(q))))
+//};
+export const queueManip : State<Queue, number> = bind(bind(dequeue, (x: number) => bind(enqueue(2*x), (y: undefined) => enqueue(x/3))), (x: undefined) => dequeue);
+
+
+console.log(queueManip([6, 7, 8]));
+
--- hw/assignment1/src/part3/state.ts
+++ hw/assignment1/src/part3/state.ts
@@ -4,7 +4,7 @@ export type State<S, A> = (initialState: S) => [S, A];
 
 export const bind: <S, A, B>(state: State<S, A>, f: (x: A) => State<S, B>) => State<S, B> = <S, A, B>(state: State<S, A>,f: (x:A)=>State<S, B>) => {
     const func = (x:S):[S,B] =>{
-        return f(state(x)[1])(x);
+        return f(state(x)[1])(state(x)[0]);
     };
     return func;
 }
