90cf56c3cc673b54da09734090092fadc3c1215c
Nir <nirsel@post.bgu.ac.il>
2021-04-10 13:36:22 +0300

    update

--- hw/ex2/src/q3.ts
+++ hw/ex2/src/q3.ts
@@ -1,9 +1,9 @@
-import { CExp, Binding, ClassExp, ProcExp,  Exp, Program, makeIfExp, parseL31CExp, parseL31, IfExp, makeProcExp, parseSExp, isClassExp, makeProgram, isProgram, isAtomicExp, isCompoundExp, isBinding, isBoolExp, isLetExp, isNumExp, isPrimOp, isProcExp, isStrExp, isVarDecl, isVarRef } from "./L31-ast";
+import { unparseL31,makeVarDecl, DefineExp, isAppExp, isCExp, isDefineExp, isIfExp, isLitExp, makeLetExp,Binding, makeDefineExp, makeAppExp, CExp, ClassExp, ProcExp,  Exp, Program, makeIfExp, parseL31CExp, parseL31, IfExp, makeProcExp, parseSExp, isClassExp, makeProgram, isProgram, isAtomicExp, isCompoundExp, isBinding, isBoolExp, isLetExp, isNumExp, isPrimOp, isProcExp, isStrExp, isVarDecl, isVarRef, makeLitExp, makeBoolExp, AppExp, makePrimOp, makeVarRef } from "./L31-ast";
 import { Result, makeFailure, mapResult,makeOk, bind } from "../shared/result";
-import { map, zipWith } from "ramda";
+import { map, zipWith, reduce } from "ramda";
 import { parse as p, isSexpString, isToken } from "../shared/parser";
 import { rest } from "../shared/list";
-import { isAppExp, isCExp, isDefineExp, isIfExp, isLitExp } from "../imp/L3-ast";
+import {makeSymbolSExp  } from "../imp/L3-value";
 
 
 /*
@@ -12,17 +12,22 @@ Signature: for2proc(classExp)
 Type: ClassExp => ProcExp
 */
 
-export const makeBody = (exp: Binding[]) : Result<CExp[]> =>{
-    const cexp = mapResult((b:Binding)=>bind(p('eq? msg '+b.var.var),parseL31CExp),exp);
-    if (cexp.tag==='Failure')
-        return makeFailure('failure');
-    return makeOk(cexp.value);
+export const makeBody = (exp: Binding[],i:number) : CExp =>{
+    if (i===exp.length)
+        return makeBoolExp(false);
+    const app:AppExp = makeAppExp(makePrimOp('eq?'),[makeVarRef('msg'),makeLitExp(makeSymbolSExp(exp[i].var.var))]);
+    const then:Result<CExp> = bind(p('('+unparseL31(exp[i].val)+' )'),parseL31CExp);
+    const alt:CExp = makeBody(exp,i+1);
+    if (then.tag==="Ok")
+        return makeIfExp(app,then.value,alt);
+    return makeBoolExp(false);
+    
 }
 export const class2proc = (exp: ClassExp): ProcExp =>{
-    const res = makeBody(exp.methods);
-    if (res.tag==="Ok")
-        return makeProcExp(exp.fields, res.value);
-    return makeProcExp(exp.fields,[]);
+    if (exp.methods.length===0)
+        return makeProcExp(exp.fields,[]);
+    const res = makeProcExp([makeVarDecl('msg')],[makeBody(exp.methods,0)]);
+    return makeProcExp(exp.fields,[res]);
 }
 
     
@@ -34,26 +39,44 @@ Type: [Exp | Program] => Result<Exp | Program>
 */
 export const L31ToL3 = (exp: Exp | Program): Result<Exp | Program> => {
     if (exp.tag==="Program")
-        return makeOk(makeProgram(map((x:Exp)=>reWriteClass(x),exp.exps)));
-    return makeOk(reWriteClass(exp));
+        return makeOk(makeProgram(map(rewriteAllClass, exp.exps)));
+    return makeOk(rewriteAllClass(exp));
 }
 
-export const reWriteClass = (e: Exp):Exp=>
-isDefineExp(e)? e:
-isNumExp(e)? e:
-isBoolExp(e)? e: 
-isStrExp(e)? e: 
-isPrimOp(e)? e:
-isVarRef(e)? e:
-isVarDecl(e)? e: 
-isAppExp(e)? e: 
-isIfExp(e)? e: 
-isProcExp(e)? e: 
-isBinding(e)? e: 
-isLetExp(e)? e: 
-isLitExp(e)? e: 
-isClassExp(e)? class2proc(e):
-e
+const rewriteAllClass = (e: Exp): Exp =>
+    isDefineExp(e)? makeDefineExp(e.var,reWriteCExp(e.val)):
+    isBoolExp(e) ? e :
+    isNumExp(e) ? e :
+    isPrimOp(e) ? e :
+    isVarRef(e) ? e :
+    isVarDecl(e) ? e :
+    isIfExp(e) ?  (makeIfExp(reWriteCExp(e.test),
+                                      reWriteCExp(e.then),
+                                      reWriteCExp(e.alt))) :
+    isAppExp(e) ? makeAppExp(reWriteCExp(e.rator), map(reWriteCExp, e.rands)) :
+    isProcExp(e)? makeProcExp(e.args, map(reWriteCExp,e.body)):
+    isClassExp(e) ? class2proc(e):
+    isLetExp(e)? makeLetExp(e.bindings,map(reWriteCExp,e.body)):
+    isLitExp(e) ? e: 
+    e;
+
+const reWriteCExp = (e:CExp):CExp=>
+isBoolExp(e) ? e :
+isNumExp(e) ? e :
+isPrimOp(e) ? e :
+isVarRef(e) ? e :
+isVarDecl(e) ? e :
+isIfExp(e) ?  (makeIfExp(reWriteCExp(e.test),
+                                  reWriteCExp(e.then),
+                                  reWriteCExp(e.alt))) :
+isAppExp(e) ? makeAppExp(reWriteCExp(e.rator), map(reWriteCExp, e.rands)) :
+isProcExp(e)? makeProcExp(e.args, map(reWriteCExp,e.body)):
+isClassExp(e) ? class2proc(e):
+isLetExp(e)? makeLetExp(e.bindings,map(reWriteCExp,e.body)):
+isLitExp(e) ? e: 
+e;
+
+    
 
 
     
--- hw/ex2/src/q4.ts
+++ hw/ex2/src/q4.ts
@@ -1,10 +1,45 @@
-import { Exp, Program } from '../imp/L3-ast';
-import { Result, makeFailure } from '../shared/result';
 
+import { Result, makeFailure, makeOk } from '../shared/result';
+import { unparseL31,makeVarDecl, DefineExp, isAppExp, isCExp, isDefineExp, isIfExp, isLitExp, makeLetExp,Binding, makeDefineExp, makeAppExp, CExp, ClassExp, ProcExp,  Exp, Program, makeIfExp, parseL31CExp, parseL31, IfExp, makeProcExp, parseSExp, isClassExp, makeProgram, isProgram, isAtomicExp, isCompoundExp, isBinding, isBoolExp, isLetExp, isNumExp, isPrimOp, isProcExp, isStrExp, isVarDecl, isVarRef, makeLitExp, makeBoolExp, AppExp, makePrimOp, makeVarRef, PrimOp } from "./L31-ast";
+import { map, zipWith, reduce } from "ramda";
 /*
 Purpose: Transform L2 AST to Python program string
 Signature: l2ToPython(l2AST)
 Type: [EXP | Program] => Result<string>
 */
-export const l2ToPython = (exp: Exp | Program): Result<string>  => 
-    makeFailure("TODO");
+export const l2ToPython = (exp: Exp | Program): Result<string>  => {
+    if (exp.tag==="Program")
+        return makeOk(exp.exps.reduce((acc,curr)=>acc.concat(reWritePython(curr)+'\n'),"").slice(0,-1));
+    return makeOk(reWritePython(exp));
+}
+
+export const reWritePython = (e:Exp):string=>
+isDefineExp(e)? e.var.var+' = '+reWritePython(e.val): 
+isBoolExp(e) ?unparseL31(e) :
+isNumExp(e) ? unparseL31(e) :
+isPrimOp(e) ? reWritePrim(e) :
+isVarRef(e) ? unparseL31(e) :
+isVarDecl(e) ? unparseL31(e) :
+isStrExp(e)? e.val:
+isIfExp(e) ?  '('+reWritePython(e.then)+' if '+reWritePython(e.test)+' else '+reWritePython(e.alt)+')':
+isAppExp(e) ? reWriteApp(e):
+isProcExp(e)? '(lambda '+e.args.reduce((acc,curr)=>acc.concat(curr.var+','),"").slice(0,-1)+' : '+e.body.reduce((acc,curr)=>acc.concat(reWritePython(curr)+' '),"").slice(0,-1)+')':
+isLetExp(e)? unparseL31(e):
+unparseL31(e);
+
+export const reWritePrim = (e:PrimOp):string=>{
+    if (e.op==='=')
+        return '==';
+    return e.op;
+}
+
+export const reWriteApp = (e:AppExp):string=>{
+    if (isPrimOp(e.rator)){
+         if (unparseL31(e.rator)==='=')
+            return '('+e.rands.map((exp:CExp)=>reWritePython(exp)+' '+reWritePython(e.rator)).join(' ').slice(0,-3)+')';
+        if (unparseL31(e.rator)==='not')
+            return '('+unparseL31(e.rator)+' '+reWritePython(e.rands[0])+')';
+        return '('+e.rands.map((exp:CExp)=>reWritePython(exp)+' '+reWritePython(e.rator)).join(' ').slice(0,-2)+')'; }
+    return reWritePython(e.rator)+'('+e.rands.reduce((acc,curr)=>acc.concat(reWritePython(curr)+','),"").slice(0,-1)+')';
+}
+
